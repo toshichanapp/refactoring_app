9478f4b30ad32c428f724e5686a2fd0094f836e0
fuzisan <toshichanapp@gmail.com>
2018-05-16 01:25:10 +0000

    不要なコメントを削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,18 +9,15 @@ class FavoritesController < ApplicationController
     set_picture
     favorite = current_user.favorites.create(picture_id: params[:picture_id])
       redirect_to pictures_path
-    # redirect_to picture_path(params[:picture_id]), notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
   end
 
   def destroy
-    # set_picture
     favorite = current_user.favorites.find_by(picture_id: params[:id]).destroy
     if params[:content]
       redirect_to favorites_path
     else
       redirect_to pictures_path
     end
-    # redirect_to picture_path(params[:picture_id]), notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"
   end
 
   private
