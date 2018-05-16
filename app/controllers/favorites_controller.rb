b242a3c95ff6e8ff8ee09ee9b12e400adcec63dc
fuzisan <toshichanapp@gmail.com>
2018-05-16 01:26:52 +0000

    使っていないローカル変数を削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,12 +7,12 @@ class FavoritesController < ApplicationController
 
   def create
     set_picture
-    favorite = current_user.favorites.create(picture_id: params[:picture_id])
-      redirect_to pictures_path
+    current_user.favorites.create(picture_id: params[:picture_id])
+    redirect_to pictures_path
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(picture_id: params[:id]).destroy
+    current_user.favorites.find_by(picture_id: params[:id]).destroy
     if params[:content]
       redirect_to favorites_path
     else
